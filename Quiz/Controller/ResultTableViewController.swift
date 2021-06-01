6767decc3ce7f109e38f5ad770104ffc512ac82a
Rushabh Singh <rushabh.singh@carwale.com>
2021-06-01 22:57:05 +0530

    Title : Create Result list view controller datasource

---
+++ Quiz/Controller/ResultTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultTableViewController.swift
+//  Quiz
+//
+//  Created by Rushabh Singh on 30/05/21.
+//
+
+import UIKit
+
+class ResultTableViewController: UIViewController {
+    
+    let RESULT_TITLE = "Result"
+    let RESULT_TABLE_VIEW_CELL_IDENTIFIER = "ResultTableViewCellIdentifier"
+    var questions = [QuizViewModel]()
+    
+    @IBOutlet weak var resultTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpNavigationItem()
+        self.setUpTableView()
+    }
+    
+    private func setUpNavigationItem(){
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Restart", style: .done, target: self, action: #selector(RestartQuiz))
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        self.title = RESULT_TITLE
+    }
+    
+    @objc func RestartQuiz(sender: UIBarButtonItem) {
+        DispatchQueue.main.async {
+            if let questionVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "QuestionViewControllerIdentifier") as? QuestionViewController {
+                self.navigationController?.pushViewController(questionVC, animated: true)
+                self.navigationController?.viewControllers.remove(at: 0)
+            }
+        }
+    }
+    
+    private func setUpTableView() {
+        resultTableView.dataSource = self
+    }
+    
+}
---
+++ Quiz/Controller/ResultTableViewControllerDataSource.swift
@@ -0,0 +1,58 @@
+//
+//  ResultTableViewControllerDataSource.swift
+//  Quiz
+//
+//  Created by Rushabh Singh on 01/06/21.
+//
+
+import UIKit
+
+extension ResultTableViewController : UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        self.questions.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if self.questions.count > section{
+            return  self.questions[section].questionText
+        }
+        return ""
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.questions.count > section{
+            return  self.questions[section].options.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell : UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: RESULT_TABLE_VIEW_CELL_IDENTIFIER)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: RESULT_TABLE_VIEW_CELL_IDENTIFIER)
+        }
+        if indexPath.section < self.questions.count{
+            let option = self.questions[indexPath.section].options[indexPath.row]
+            cell?.textLabel!.text = option.text
+            let userAnswers = self.questions[indexPath.section].userAnswers
+            let correctAnswers = self.questions[indexPath.section].correctAnswerIds
+            cell?.textLabel!.font = UIFont.boldSystemFont(ofSize: 14)
+            if userAnswers.contains(option.id) {
+                if correctAnswers.contains(option.id) {
+                    cell?.textLabel?.textColor = UIColor.green
+                }else{
+                    cell?.textLabel?.textColor = UIColor.red
+                }
+            }
+            else{
+                cell?.textLabel?.textColor = UIColor.gray
+            }
+        }
+        cell?.textLabel?.numberOfLines = 0
+        
+        return cell!
+    }
+    
+}
+
